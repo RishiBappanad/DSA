29ce80de36d54f4ceeb935c113b9ce64935e543f
RishiBappanad <65369047+RishiBappanad@users.noreply.github.com>
2026-03-17 18:02:33 -0400

    Time: 737 ms (5.22%) | Memory: 40.9 MB (13.91%) - LeetSync

---
+++ 1709-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.py
@@ -0,0 +1,25 @@
+from datetime import datetime, timedelta
+from collections import deque
+class Solution:
+    def alertNames(self, keyName: List[str], keyTime: List[str]) -> List[str]:
+        hashmap = {} #keep rolling list of last 3 scans
+        res = []
+        hour = timedelta(hours=1)
+        for i in range(len(keyName)):
+            name = keyName[i]
+            time = keyTime[i]
+            if name not in hashmap:
+                hashmap[name] = [time]
+            else:
+                hashmap[name].append(time)
+        for i in hashmap.keys():
+            hashmap[i] = sorted(hashmap[i])
+            for j in range(len(hashmap[i]) - 2):
+                times = hashmap[i]
+                first = datetime.strptime(times[j], "%H:%M")
+                recent = datetime.strptime(times[j + 2], "%H:%M")
+                if recent - first <= hour:
+                    res.append(i)
+                    break
+        print(hashmap)
+        return sorted(res)
