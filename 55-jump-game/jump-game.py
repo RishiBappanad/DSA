570ee5ef1f4462a2ad5b07356edddfa2b64fc449
RishiBappanad <65369047+RishiBappanad@users.noreply.github.com>
2026-03-17 21:23:10 -0400

    Time: 11 ms (91.88%) | Memory: 20.3 MB (60.63%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,24 +1,11 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        if len(nums) == 1:
-            return True
-        if nums[0] == False:
-            return False
-        dp = [None] * len(nums)
-        def dfs(i): 
-            jumps = nums[i]
-            for j in range(jumps, -1, -1):
-                if i + j >= len(nums):
-                    dp[i] = True
-                    return True
-                if dp[i + j]:
-                    dp[i] = True
-                    return True
-            dp[i] = False
-            return False
-        for i in range(len(nums) - 1, -1, -1):
-            if i + nums[i] >= len(nums) - 1:
-                dp[i] = True
-            dfs(i)
-        print(dp)
-        return dp[0]
+        gas = 0
+        for n in nums:
+            if gas < 0:
+                return False
+            elif n > gas:
+                gas = n
+            gas -= 1
+            
+        return True
