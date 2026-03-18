ff65266547a4e2b171da70a92814caa09995b934
RishiBappanad <65369047+RishiBappanad@users.noreply.github.com>
2026-03-17 20:10:19 -0400

    Time: 246 ms (53.94%) | Memory: 21.3 MB (99.31%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,41 +1,18 @@
 class Solution(object):
     def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
-
-        islands = {}
-        self.count = 0
-        def bfs(i, j, parent):
-            if grid[i][j] == "0":
+        row,col=len(grid),len(grid[0])
+        count=0
+        def dfs(r,c):
+            if r<0 or c<0 or r==row or c==col or grid[r][c]=="0":
                 return
-            if (i, j) in islands:
-                return
-            islands[(i, j)] = 1
-            if not parent:
-                self.count += 1
-            if i > 0:
-                up = int(grid[i-1][j])
-                if up:
-                    bfs(i-1, j, True)
-            if j > 0:
-                left = int(grid[i][j-1])
-                if left:
-                    bfs(i, j-1, True)
-            if i < len(grid) - 1:
-                down = int(grid[i + 1][j])
-                if down:
-                    bfs(i+1, j, True)
-            if j < len(grid[i]) - 1:
-                right = int(grid[i][j+1])
-                if right:
-                    bfs(i, j+1, True)
-
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                if (i, j) not in islands and grid[i][j] == "1":
-                    bfs(i, j, False)
-                print(self.count, i, j)
-        return self.count
-            
+            grid[r][c]="0"
+            dfs(r-1,c)
+            dfs(r,c-1)
+            dfs(r+1,c)
+            dfs(r,c+1)
+        for r in range(row):
+            for c in range(col):
+                if grid[r][c]=="1":
+                    dfs(r,c)
+                    count+=1
+        return count
